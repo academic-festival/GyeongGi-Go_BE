78d30eb8bd3e1afcc24793d1247104cdba22266d
TaeRim0701 <xofla71@naver.com>
2025-10-01 13:57:23 +0900

    initial project

---
+++ src/main/java/academic_festival/gyeonggi_go/GyeonggiGoApplication.java
@@ -0,0 +1,13 @@
+package academic_festival.gyeonggi_go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyeonggiGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GyeonggiGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/academic_festival/gyeonggi_go/GyeonggiGoApplicationTests.java
@@ -0,0 +1,13 @@
+package academic_festival.gyeonggi_go;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyeonggiGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
