a3dcb3be394479044802200b65c3ad3ac8caacbb
mingMINJOO <smj8223@naver.com>
2025-10-08 00:27:31 +0900

    [refactor/#1] request 수정

--- src/main/java/academic_festival/gyeonggi_go/chatbot/Dto/Request/ChatbotRequestDto.java
+++ src/main/java/academic_festival/gyeonggi_go/chatbot/Dto/Request/ChatbotRequestDto.java
@@ -3,7 +3,7 @@ package academic_festival.gyeonggi_go.chatbot.Dto.Request;
 import java.util.List;
 
 public class ChatbotRequestDto {
-    private String place;
+    private String placename;
     private String question;
     private List<String> conversationHistory; // 선택 사항: 대화 기록
 
@@ -13,19 +13,17 @@ public class ChatbotRequestDto {
 
     // 모든 필드를 포함하는 생성자 (선택 사항)
     public ChatbotRequestDto(String place, String question, List<String> conversationHistory) {
-        this.place = place;
+        this.placename = place;
         this.question = question;
         this.conversationHistory = conversationHistory;
     }
 
     // Getter와 Setter
     public String getPlace() {
-        return place;
+        return placename;
     }
 
-    public void setPlace(String place) {
-        this.place = place;
-    }
+    public void setPlace(String place) { this.placename = placename; }
 
     public String getQuestion() {
         return question;
