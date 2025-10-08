cf8283bdbec060c4be70038b2b5cac73446c874c
TaeRim0701 <xofla71@naver.com>
2025-10-08 23:26:43 +0900

    [feat] DB 구축 ETL 배치 동기화

--- src/main/java/academic_festival/gyeonggi_go/GyeonggiGoApplication.java
+++ src/main/java/academic_festival/gyeonggi_go/GyeonggiGoApplication.java
@@ -2,7 +2,9 @@ package academic_festival.gyeonggi_go;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class GyeonggiGoApplication {
 
