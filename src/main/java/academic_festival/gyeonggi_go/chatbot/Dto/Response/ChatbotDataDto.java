d611909f3c8e872280eb5f323ed7a7bc2301358b
mingMINJOO <smj8223@naver.com>
2025-10-31 18:36:45 +0900

    [feat/#06] response 수정

--- src/main/java/academic_festival/gyeonggi_go/chatbot/Dto/Response/ChatbotDataDto.java
+++ src/main/java/academic_festival/gyeonggi_go/chatbot/Dto/Response/ChatbotDataDto.java
@@ -1,5 +1,6 @@
 package academic_festival.gyeonggi_go.chatbot.Dto.Response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,5 +10,7 @@ import java.util.List;
 @AllArgsConstructor
 public class ChatbotDataDto {
     private final String answer;
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY) //값이 null이면 필드를 아예 제외
     private final List<String> suggestedQuestions;
 }
