1c4a4054db12ae775006fbfbedd49a80a2c9b582
mingMINJOO <smj8223@naver.com>
2025-11-12 22:05:24 +0900

    [fix/#33] csrf 보호기능 비활성화

--- src/main/java/academic_festival/gyeonggi_go/config/SecurityConfig.java
+++ src/main/java/academic_festival/gyeonggi_go/config/SecurityConfig.java
@@ -13,6 +13,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/home", "/places/**", "/chatbot/**").permitAll()
                         .anyRequest().authenticated() // 나머지 요청은 인증 필요
