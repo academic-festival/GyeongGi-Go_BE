1b789deb4869ffc62541a567d1fb06e2684f07bf
mingMINJOO <smj8223@naver.com>
2025-11-12 22:42:25 +0900

    [feat/#33] cors에러

--- src/main/java/academic_festival/gyeonggi_go/config/SecurityConfig.java
+++ src/main/java/academic_festival/gyeonggi_go/config/SecurityConfig.java
@@ -4,7 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
 
 @Configuration
 @EnableWebSecurity
@@ -14,11 +21,29 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                // 4. 세션 정책을 STATELESS로 설정 (API 서버이므로)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/swagger-ui/**", "/swagger", "v2/api-docs", "/v3/api-docs/**", "/swagger-ui.html", "api-docs", "/api-docs/**", "/configuration/security", "/configuration/ui", "/swagger-resources/**", "/webjars/**",
+                        .requestMatchers("/swagger-ui/**", "/swagger", "/v2/api-docs", "/v3/api-docs/**", "/swagger-ui.html", "/api-docs", "/api-docs/**", "/configuration/security", "/configuration/ui", "/swagger-resources/**", "/webjars/**",
                                 "/home", "/places/**", "/chatbot/**").permitAll()
                         .anyRequest().authenticated() // 나머지 요청은 인증 필요
                 );
         return http.build();
+    }
+
+    //cors
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOriginPatterns(Arrays.asList("*")); // 모든 Origin 허용 (운영에선 구체적으로 명시)
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 위 설정 적용
+        return source;
     }
 }
