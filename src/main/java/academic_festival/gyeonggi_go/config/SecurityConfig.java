8e3dfd3e83ecf71e05d433d222dbfeba094239b9
mingMINJOO <smj8223@naver.com>
2025-11-12 21:52:23 +0900

    [feat/#33] spring security

---
+++ src/main/java/academic_festival/gyeonggi_go/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package academic_festival.gyeonggi_go.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/home", "/places/**", "/chatbot/**").permitAll()
+                        .anyRequest().authenticated() // 나머지 요청은 인증 필요
+                );
+        return http.build();
+    }
+}
